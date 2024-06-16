4bd7df0621c302f7eef89829c820fc8504d87cd1
Carlos Fermino <brutosacf@gmail.com>
2024-06-16 17:35:10 -0300

    Criado anotações no readme

--- src/components/form/inputWithSuggestions/InputWithSuggestions.jsx
+++ src/components/form/inputWithSuggestions/InputWithSuggestions.jsx
@@ -7,6 +7,7 @@ const SuggestionsContainer = styled.div`
   margin-top: 10px;
   background-color: ${props => props.theme.colors.white};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  position: absolute;
 `
 
 const Suggestions = styled.div`
